d7d03a3ce5118c17d823bc882223b280a6eb1c71
dpj1951 <ggdpjohnson@gmail.com>
2026-03-26 11:44:33 -0400

    fix: add send_file to Flask imports to fix export CSV 500 error

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, abort, jsonify
+from flask import Flask, render_template, request, redirect, url_for, abort, jsonify, send_file
 import json
 import os
 import uuid
